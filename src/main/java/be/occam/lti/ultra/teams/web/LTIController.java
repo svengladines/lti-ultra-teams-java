6745bfa346ce8a78548b3a9929b64e835707a126
Sven Gladines <sven.gladines@gmail.com>
2024-12-10 14:55:13 +0100

    Update LTIController.java
    Fix import

--- src/main/java/be/occam/lti/ultra/teams/web/LTIController.java
+++ src/main/java/be/occam/lti/ultra/teams/web/LTIController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.Controller;
+import org.springframework.stereotype.Controller;
 
 import java.net.URI;
 
