7504a3f35d19ec976fb62746e907ee16b9d7587e
Sven Gladines <sven.gladines@gmail.com>
2025-01-15 15:07:13 +0100

    fix local properties

--- src/main/java/be/occam/lti/ultra/teams/UltraTeamsApplication.java
+++ src/main/java/be/occam/lti/ultra/teams/UltraTeamsApplication.java
@@ -1,6 +1,7 @@
 package be.occam.lti.ultra.teams;
 
 import be.occam.lti.ultra.teams.config.SystemProperties;
+import be.occam.lti.ultra.teams.config.feature.LocalProperties;
 import be.occam.lti.ultra.teams.infrastructure.microsoft.MicrosoftProperties;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({SystemProperties.class,MicrosoftProperties.class})
+@EnableConfigurationProperties({SystemProperties.class,MicrosoftProperties.class, LocalProperties.class})
 public class UltraTeamsApplication {
 
     public static void main(String[] args) {
--- src/main/java/be/occam/lti/ultra/teams/config/feature/LocalProperties.java
+++ src/main/java/be/occam/lti/ultra/teams/config/feature/LocalProperties.java
@@ -1,4 +1,7 @@
 package be.occam.lti.ultra.teams.config.feature;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "occam.lti.ultra.feature.local")
 public record LocalProperties(boolean enabled) {
 }
